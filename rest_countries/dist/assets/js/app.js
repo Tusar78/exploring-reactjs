db4fcb463cdc3885d2f25b2bae5b0d93765de0bc
Tusar <tusarahmed141@gmail.com>
2022-07-08 21:15:28 +0600

    Normal perameter to destructuring

--- rest_countries/dist/assets/js/app.js
+++ rest_countries/dist/assets/js/app.js
@@ -15,13 +15,13 @@ const displayCountries = (countries) => {
   countryContainer.innerHTML = countriesToHTML.join(' ');
 };
 
-const getCountry = (country) => {
+const getCountry = ({flags, name, capital, population}) => {
   return `
     <div class="country__card max-w-xs border border-blue-200 rounded-sm shadow-md p-2">
-      <img src="${country.flags.png}" class="country__flag w-52 h-auto object-cover" alt="${country.name.common}">
-      <h2 class="country__title">Name: ${country.name.common}</h2>
-      <span class="country__subtitle">Capital: ${country.capital}</span>
-      <p class="country__info">Population: ${country.population}</p>
+      <img src="${flags.png}" class="country__flag w-52 h-auto object-cover" alt="${name.common}">
+      <h2 class="country__title">Name: ${name.common}</h2>
+      <span class="country__subtitle">Capital: ${capital}</span>
+      <p class="country__info">Population: ${population}</p>
     </div>
   `;
 };
