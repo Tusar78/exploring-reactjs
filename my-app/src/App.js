6429878f3fd1c28eddd0efe35f0556e7a80a7d1b
Tusar <tusarahmed141@gmail.com>
2022-07-09 23:59:57 +0600

    Explore useeffect hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,32 +3,30 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
   return (
     <div className="container">
       <div className="card__container">
-        <Counter></Counter>
+        {users.map((user) => {
+          return <Users key={user.id} name={user.name} website={user.website} ></Users>;
+        })}
       </div>
     </div>
   );
 }
 
-// Counter increased and decreased
-function Counter() {
-  const [count, setCount] = useState(1);
-  const increasedCount = () => {
-    const increased = count + 1;
-    setCount(increased);
-  }
-  
-  const decreasedCount = () => setCount(count - 1);
-
+function Users({name, website}) {
   return (
     <div className="product">
-      <h2 className="product__name">Counter: {count}</h2>
-      <button className="btn" onClick={increasedCount}>Increased</button>
-      <button className="btn" onClick={decreasedCount}>Decreased</button>
+      <h2 className="product__name">{name}</h2>
+      <span className="product__price">{website}</span>
     </div>
-  )
+  );
 }
 
 export default App;
