84eddc4dc9c981824855148ed880b8f85820e120
Tusar <tusarahmed141@gmail.com>
2022-07-09 23:02:20 +0600

    Similar in look similar in data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,30 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import "./App.css";
-import { useState } from "react";
 
 function App() {
   return (
     <div className="container">
       <div className="card__container">
-        <Counter></Counter>
+        <Employee></Employee>
+        <Employee></Employee>
+        <Employee></Employee>
+        <Employee></Employee>
+        <Employee></Employee>
+        <Employee></Employee>
       </div>
     </div>
   );
 }
 
-function Counter() {
-  const [count, setCount] = useState(1);
-  const increasedCount = () => {
-    const increased = count + 1;
-    setCount(increased);
-  }
-
-  const decreasedCount = () => setCount(count - 1);
-
+// Similar in look similar in data component
+function Employee() {
   return (
     <div className="product">
-      <h2>Count: {count}</h2>
-      <button className="btn" onClick={increasedCount}>Increased</button>
-      <button className="btn" onClick={decreasedCount}>Decreased</button>
+      <h2 className="product__name">I'm Tusar</h2>
+      <span className="product__price">Frontend Developer!</span>
     </div>
-  )
+  );
 }
 
 export default App;
