4d6b7fd7b5558754560686363b8f44a87d2e7da1
Tusar <tusarahmed141@gmail.com>
2022-07-09 16:06:57 +0600

    Generate dynamic data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const employee = {
+  //   empOne: {name: 'Tusar', role: 'Frontend developer'},
+  //   empTwo: {name: 'Rakib', role: 'IT-Officer'},
+  //   empThree: {name: 'Sharif', role: 'Businessman'},
+  // }
+  const employee = ['Mastura', 'Borsha', 'Rumi', 'Sabrina', 'Munjila', 'Tusar'];
   return (
     <div className="container">
       <div className="card__container">
-        <Person name="Tusar" rol="Professional Web developer"></Person>
-        <Person name="Rakibul Islam" rol="IT-Officer"></Person>
-        <Person name="Sharif" rol="Business man"></Person>
+        {
+          employee.map(emp => {
+            return <Person key={emp} name={emp}></Person>
+          })
+        }
+        {/* <Person name={employee.empOne.name} rol={employee.empOne.role}></Person>
+        <Person name={employee.empTwo.name} rol={employee.empTwo.role}></Person>
+        <Person name={employee.empThree.name} rol={employee.empThree.role}></Person> */}
+        
       </div>
     </div>
   );
 }
 
-function Person({name, rol}) {
+function Person({name}) {
   return (
     <div className="person">
       <img className="person__img" src={logo} alt="person-img" />
       <h2 className="person__title">I'm {name}</h2>
-      <span className="person__role">{rol}</span>
+      {/* <span className="person__role">{rol}</span> */}
     </div>
   )
 }
