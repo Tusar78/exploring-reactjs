c9230665d9fda96c197658468f56255ab2bf1fca
Tusar <tusarahmed141@gmail.com>
2022-07-09 19:51:20 +0600

    Explore hook, name useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   // const employee = [
@@ -8,48 +9,52 @@ function App() {
   //   {name: 'Sharif', role: 'Businessman'}
   // ]
 
-  const products = [
-    {
-      id: 1,
-      name: "Canon EOS 6D Mark",
-      price: "115,000৳",
-      img: "https://www.startech.com.bd/image/cache/catalog/camera/dslr-camera/canon/eos-6d-mark-ii%20/eos-6d-mark-ii-01-500x500.jpg",
-    },
-    {
-      id: 2,
-      name: "HP 15s-du1087TU",
-      price: "35,500৳",
-      img: "https://www.startech.com.bd/image/cache/catalog/laptop/hp-laptop/15s-du1087tu/15s-du1087tu-01-500x500.jpg",
-    },
-    {
-      id: 3,
-      name: "Havit 2218D 3.5mm",
-      price: "580৳",
-      img: "https://www.startech.com.bd/image/cache/catalog/headphone/havit/h2218d/h2218d-500x500.jpg",
-    },
-    {
-      id: 4,
-      name: "Realme DIZO Watch",
-      price: "3,990৳",
-      img: "https://www.startech.com.bd/image/cache/catalog/smart-watch/realme/dizo-watch-2/dizo-watch-2-pink-01-500x500.jpg",
-    },
-  ];
+  // const products = [
+  //   {
+  //     id: 1,
+  //     name: "Canon EOS 6D Mark",
+  //     price: "115,000৳",
+  //     img: "https://www.startech.com.bd/image/cache/catalog/camera/dslr-camera/canon/eos-6d-mark-ii%20/eos-6d-mark-ii-01-500x500.jpg",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "HP 15s-du1087TU",
+  //     price: "35,500৳",
+  //     img: "https://www.startech.com.bd/image/cache/catalog/laptop/hp-laptop/15s-du1087tu/15s-du1087tu-01-500x500.jpg",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Havit 2218D 3.5mm",
+  //     price: "580৳",
+  //     img: "https://www.startech.com.bd/image/cache/catalog/headphone/havit/h2218d/h2218d-500x500.jpg",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Realme DIZO Watch",
+  //     price: "3,990৳",
+  //     img: "https://www.startech.com.bd/image/cache/catalog/smart-watch/realme/dizo-watch-2/dizo-watch-2-pink-01-500x500.jpg",
+  //   },
+  // ];
 
   return (
     <div className="container">
       <div className="card__container">
-        {products.map((product) => {
-          return (
-            <Product
-              key={product.id}
-              name={product.name}
-              price={product.price}
-              img={product.img}
-            >
-            </Product>
-          );
-        })}
+        <Counter></Counter>
       </div>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(5);
+  const increasedCount = () => setCount(count + 1);
+  const decreasedCount = () => setCount(count - 1);
+
+  return (
+    <div className="product">
+      <h2>Count: {count}</h2>
+      <button className="btn" onClick={increasedCount}>Increased</button>
+      <button className="btn" onClick={decreasedCount}>Decreased</button>
     </div>
   );
 }
@@ -64,14 +69,14 @@ function App() {
 //   )
 // }
 
-function Product({ name, price, img }) {
-  return (
-    <div className="product">
-      <img src={img} alt={name} className="product__img" />
-      <h2 className="product__name">{name}</h2>
-      <p className="product__price">Price: {price}</p>
-    </div>
-  );
-}
+// function Product({ name, price, img }) {
+//   return (
+//     <div className="product">
+//       <img src={img} alt={name} className="product__img" />
+//       <h2 className="product__name">{name}</h2>
+//       <p className="product__price">Price: {price}</p>
+//     </div>
+//   );
+// }
 
 export default App;
