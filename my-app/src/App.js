769c5b837c891a0d8d30baec110281737afd1ba5
Tusar <tusarahmed141@gmail.com>
2022-07-12 20:50:04 +0600

    Load data using react useffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,28 +3,56 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([]);
+  // Data in or not
+  const [countries, setCountries] = useState([]);
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
+    fetch(`https://restcountries.com/v3.1/all`)
       .then((res) => res.json())
-      .then((data) => setUsers(data));
+      .then((data) => setCountries(data));
   }, []);
   return (
-    <div className="container">
-      <div className="card__container">
-        {users.map((user) => {
-          return <Users key={user.id} name={user.name} website={user.website} ></Users>;
-        })}
-      </div>
-    </div>
+    <>
+      <Header></Header>
+      <section className="container">
+        <div className="card__container">
+          {countries.map((country) => {
+            return (
+              <Countries
+                key={country.name.common}
+                name={country.name.common}
+                population={country.population}
+              ></Countries>
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
 }
 
-function Users({name, website}) {
+// Header
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <h1 className="nav__logo">Rest Countries</h1>
+
+        <ul className="nav__list">
+          <li className="nav__item">Home</li>
+          <li className="nav__item">Countries</li>
+          <li className="nav__item">About us</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+// External data
+function Countries({name, population}) {
   return (
     <div className="product">
       <h2 className="product__name">{name}</h2>
-      <span className="product__price">{website}</span>
+      <span className="product__price">Population: {population}</span>
     </div>
   );
 }
