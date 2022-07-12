fbe6600e515d521ad1d643ca90b8d05f16d242c3
Tusar <tusarahmed141@gmail.com>
2022-07-13 01:58:24 +0600

    Header file include succesfully

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import Countries from "./Components/Countries/Countries";
 import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <main>
-        <Countries></Countries>
-      </main>
     </>
   );
 }
--- my-app/src/Components/Header/Header.js
+++ my-app/src/Components/Header/Header.js
@@ -1,29 +1,24 @@
 import "./Header.css";
-import React from "react";
+
+import React from 'react';
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className='header'>
       <nav className="nav">
-        <h1 className="nav__logo">Rest Countries</h1>
+        <h1 className='nav__logo'>Rest Countries</h1>
 
-        <ul className="nav__list">
+        <div className="nav__list">
           <li className="nav__item">
-            <a href="#home" className="nav__link">
-              Home
-            </a>
+            <a href="#home" className="nav__link">Home</a>
           </li>
           <li className="nav__item">
-            <a href="#countries" className="nav__link">
-              Countries
-            </a>
+            <a href="#countries" className="nav__link">Countries</a>
           </li>
           <li className="nav__item">
-            <a href="#about" className="nav__link">
-              About us
-            </a>
+            <a href="#about" className="nav__link">About us</a>
           </li>
-        </ul>
+        </div>
       </nav>
     </header>
   );
