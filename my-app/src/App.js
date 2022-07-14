9047f0d1ed85095b077e4a936f1489a8237b3c70
Tusar <tusarahmed141@gmail.com>
2022-07-14 22:47:19 +0600

    All components are delete without header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
-import Countries from "./Components/Countries/Countries";
 import Header from "./Components/Header/Header";
-import Steps from "./Components/Steps/Steps";
 
 function App() {
   return (
     <>
       <Header></Header>
       <main className="main">
-        <Steps></Steps>
-        <Countries></Countries>
+        
       </main>
     </>
   );
