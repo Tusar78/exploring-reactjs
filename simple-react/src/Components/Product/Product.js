e5da8b4f5e47e1f8a7e43a1710fd1c664650aa95
Tusar <tusarahmed141@gmail.com>
2022-07-30 16:53:18 +0600

    Data load on dynamically

---
+++ simple-react/src/Components/Product/Product.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Product = ({product}) => {
+  const {img, name, price, seller, rating} = product;
+  return (
+    <div className='product'>
+      <img src={img} alt={name} className='product__img' />
+
+      <div className="product__details">
+        <h2 className="product__title">{name}</h2>
+        <span className='product__price'>Price: ${price}</span>
+        <small className="product__seller">Manufacturer: {seller}</small>
+        <small className="product__rating">Rating: {rating} star</small>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
