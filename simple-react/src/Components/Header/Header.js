35354d298df7c040ff544904066a16f28747a8a9
Tusar <tusarahmed141@gmail.com>
2022-07-30 12:26:12 +0600

    Done on header markup

--- simple-react/src/Components/Header/Header.js
+++ simple-react/src/Components/Header/Header.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <header>
-      <h2>This is header</h2>
-      <span>Subtitle</span>
+    <header className="header">
+      <nav className="nav container">
+        <a href="/home" className="nav__logo">
+          <img src='images/Logo.svg' alt="Website logo" className="nav__logo-img" />
+        </a>
+
+        <div className="nav__menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="/order" className="nav__link">
+                Order
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/order-review" className="nav__link">
+                Order Review
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/manage-inventory" className="nav__link">
+                Manage Inventory
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/login" className="nav__link">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 };
