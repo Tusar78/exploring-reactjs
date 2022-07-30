ddab3fe729bd1f5a00c63678c1d5fcddfefffc03
Tusar <tusarahmed141@gmail.com>
2022-07-30 12:38:54 +0600

    Navigation desing complete

--- simple-react/src/Components/Header/Header.js
+++ simple-react/src/Components/Header/Header.js
@@ -5,29 +5,29 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="/home" className="nav__logo">
+        <a href="#home" className="nav__logo">
           <img src='images/Logo.svg' alt="Website logo" className="nav__logo-img" />
         </a>
 
         <div className="nav__menu">
           <ul className="nav__list">
             <li className="nav__item">
-              <a href="/order" className="nav__link">
+              <a href="#order" className="nav__link">
                 Order
               </a>
             </li>
             <li className="nav__item">
-              <a href="/order-review" className="nav__link">
+              <a href="#order-review" className="nav__link">
                 Order Review
               </a>
             </li>
             <li className="nav__item">
-              <a href="/manage-inventory" className="nav__link">
+              <a href="#manage-inventory" className="nav__link">
                 Manage Inventory
               </a>
             </li>
             <li className="nav__item">
-              <a href="/login" className="nav__link">
+              <a href="#login" className="nav__link">
                 Login
               </a>
             </li>
