07e307a6c9d4bded76634414379debafb502de40
Tusar <tusarahmed141@gmail.com>
2022-07-30 23:00:16 +0600

    Add cart calculatore

--- simple-react/src/Components/Cart/Cart.js
+++ simple-react/src/Components/Cart/Cart.js
@@ -1,16 +1,33 @@
 import React from "react";
-import './Cart.css'
+import "./Cart.css";
 
-const Cart = () => {
+const Cart = ({ cart }) => {
+  const price = cart.reduce((prev, curr) => {
+    return prev + curr.price;
+  }, 0);
+
+  const shipping = cart.reduce((prev, curr) => {
+    return prev + curr.shipping;
+  }, 0);
+
+  const tax = cart.reduce((prev, curr) => {
+    const newTax = ((curr.price * 3) / 100).toFixed(2);
+    const numTax = parseFloat(newTax);
+    console.log(numTax);
+    return prev + numTax;
+  }, 0);
+
+  const grandTotal = price + shipping + tax; 
+
   return (
-    <>
+    <div className="cart">
       <h4 className="cart__title">Order Summery</h4>
       <div className="cart__info">
-        <p>Selected item: </p>
-        <p>Total price: </p>
-        <p>Total shipping Charge: </p>
-        <p>Tax: </p>
-        <h5 className="cart__total">Grand Total: </h5>
+        <p>Selected item: {cart.length}</p>
+        <p>Total price: ${price}</p>
+        <p>Total shipping Charge: ${shipping}</p>
+        <p>Tax: ${tax}</p>
+        <h5 className="cart__total">Grand Total: ${grandTotal}</h5>
       </div>
       <div className="cart__buttons">
         <button className="cart__btn cart__btn--red">
@@ -18,11 +35,11 @@ const Cart = () => {
           <i className="uil uil-trash cart__btn-icon"></i>
         </button>
         <button className="cart__btn cart__btn--yellow">
-        <span>Review Order</span>
+          <span>Review Order</span>
           <i className="uil uil-arrow-right cart__btn-icon"></i>
         </button>
       </div>
-    </>
+    </div>
   );
 };
 
