8cc2e419dbadf8dc41eeaa3bd7fc02c2b298cb9b
Tusar <tusarahmed141@gmail.com>
2022-07-31 20:04:33 +0600

    explore destructuring

--- simple-react-spa/src/utilities/destructuring.js
+++ simple-react-spa/src/utilities/destructuring.js
@@ -10,7 +10,22 @@ const personObject = {
 const cart = {
   abc: 1,
   def: 2,
-  ghi: 3
-}
+  ghi: 3,
+};
 
-const personArray = ['Tusar', 'Ali'];
+const personArray = ["Tusar", "Ali"];
+
+const giveQuantity = (id) => {
+  console.log(cart[id]);
+};
+
+giveQuantity("abc");
+
+const [, lastname] = personArray;
+console.log(lastname);
+
+const {
+  firstName: myName,
+  address: { country, capital },
+} = personObject;
+console.log(country, capital, myName);
