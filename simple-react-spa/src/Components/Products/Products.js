6ec329a9e148ed05509a4ad0be2b666fb4a728b3
Tusar <tusarahmed141@gmail.com>
2022-07-31 23:24:31 +0600

    Display a single cart

--- simple-react-spa/src/Components/Products/Products.js
+++ simple-react-spa/src/Components/Products/Products.js
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import Product from '../Product/Product';
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 
 const Products = () => {
   const [guns, setGuns] = useState([]);
+  const [cart, setCart] = useState([]);
+  console.log(cart);
   useEffect(() => {
-    fetch('https://raw.githubusercontent.com/mir-hussain/guns/main/data.json')
-      .then(res => res.json())
-      .then(data => setGuns(data))
-  }, [])
+    fetch("https://raw.githubusercontent.com/mir-hussain/guns/main/data.json")
+      .then((res) => res.json())
+      .then((data) => setGuns(data));
+  }, []);
 
-  const handleClick = card => {
-    console.log(card);
-  }
+  const handleClick = (card) => {
+    const newCart = [...cart, card];
+    setCart(newCart);
+  };
 
   return (
-    <section className='product__container container'>
+    <>
       {
-        guns.map(gun => {
-          return <Product key={gun.id} data={gun} handleClick={handleClick} />
-        })
+        cart.map(ct => <h2>{ct.name}</h2>)
       }
-      
-    </section>
+      <section className="product__container container">
+        {guns.map((gun) => {
+          return <Product key={gun.id} data={gun} handleClick={handleClick} />;
+        })}
+      </section>
+    </>
   );
 };
 
