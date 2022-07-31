402aed0ff69d32ef129365c20b2123fd6a47aedd
Tusar <tusarahmed141@gmail.com>
2022-07-31 16:52:58 +0600

    Create person array and objects

---
+++ simple-react-spa/src/utilities/destructuring.js
@@ -0,0 +1,16 @@
+const personObject = {
+  firstName: "Tusar",
+  lastName: "Ali",
+  address: {
+    country: "Bangladesh",
+    capital: "Dhaka",
+  },
+};
+
+const cart = {
+  abc: 1,
+  def: 2,
+  ghi: 3
+}
+
+const personArray = ['Tusar', 'Ali'];
