7c14e6b05d6cefac52be1d887449f19b5e6374e1
Tusar <tusarahmed141@gmail.com>
2022-07-31 23:45:39 +0600

    Added modal

--- simple-react-spa/src/Components/Products/Products.js
+++ simple-react-spa/src/Components/Products/Products.js
@@ -1,10 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement("#root");
 
 const Products = () => {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
   const [guns, setGuns] = useState([]);
   const [cart, setCart] = useState([]);
-  console.log(cart);
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/mir-hussain/guns/main/data.json")
       .then((res) => res.json())
@@ -16,11 +32,36 @@ const Products = () => {
     setCart(newCart);
   };
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <>
-      {
-        cart.map(ct => <h2>{ct.name}</h2>)
-      }
+      <section>
+        <button onClick={openModal}>Open Modal</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          {cart.map((ct) => (
+            <h2>{ct.name}</h2>
+          ))}
+        </Modal>
+      </section>
+
       <section className="product__container container">
         {guns.map((gun) => {
           return <Product key={gun.id} data={gun} handleClick={handleClick} />;
